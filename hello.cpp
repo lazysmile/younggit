16f8706310f40c3e128a1a2aa7b89fe55056f796
lazysmile <lazysmile@live.cn>
2012-12-24 14:13:48 +0800

    hello world 2.0

---
+++ hello.cpp
@@ -0,0 +1,24 @@
+//&>/dev/null;x="${0%.*}";[ ! "$x" -ot "$0" ]||(rm -f "$x";g++ -o "$x" "$0")&&"$x" $*;exit
+// =====================================================================================
+// 
+//       Filename:  hello.cpp
+// 
+//    Description:  hello world 2.0
+// 
+//        Version:  1.0
+//        Created:  12/24/2012 02:11:59 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Philip.Young (Cxy), yangcongxi@cdeledu.com
+//        Company:  CDEL
+// 
+// =====================================================================================
+
+#include<iostream>
+
+int main(char *argv[],int argc)
+{
+	std::cout<<"hello world 2.0"<<std::endl;
+	return 0;
+}
